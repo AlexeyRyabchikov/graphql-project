83ad0d632d09caf437ea434f657957a634e4712d
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-06-19 12:41:17 +0300

    Added mutation for article

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,8 +5,7 @@ module Types
     # They will be entry points for queries on your schema.
 
     # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
+    #field :article, ArticleType, null: false
     def all_articles
       Article.all
     end
