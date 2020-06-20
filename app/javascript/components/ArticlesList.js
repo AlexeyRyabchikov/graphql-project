30e511d808b9558f47a4be20e86e73fffe445524
AlexeyRyabchikov <al.ryabchikov@gmail.com>
2020-06-20 17:15:30 +0300

    Added react component

---
+++ app/javascript/components/ArticlesList.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class ArticlesList extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <h1> Курсы </h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default ArticlesList
